454a69cccae74c351adb6922b1f895ca3e8f0af5
UsmanHaiderWeb <usmanhaiderweb2005@gmail.com>
2026-06-20 14:52:15 +0500

    Fix project-image scale-origin consistency across 3 screen tiers
    The scale-origin (left/right reveal) was inheriting wrong on medium
    screens and image 5 wrongly scaled from the left on big screens, because
    toggling left-0/right-0 never reset the opposite side (left wins when a
    fixed-width box has both left:0 and right:0).
    
    Now each card resets the opposite side (left-auto/right-auto) per tier:
    - big   (>=800, 3 cols): scale L L L R R
    - medium(551-799, 2 cols): scale L L R L R
    - small (<=550, 1 col):   scale L R L R L

--- src/Components/pages/SideBySideProjectPage.jsx
+++ src/Components/pages/SideBySideProjectPage.jsx
@@ -10,11 +10,13 @@ return (
             <p className='hidden lg:block w-[32%] text-[19px] leading-[25px] opacity-55'>These projects primarily focuses on the javascript and react concepts and understandings.</p>
         </div>
         <div className='w-full flex justify-start items-start flex-col px-[4.2vw] micro:px-[10px]'>
+            {/* Scale-origin (L/R) per tier  -> small: L R L R L | medium: L L R L R | big: L L L R R */}
+            {/* Placement per tier           -> small: 1 col      | medium: 2 cols     | big: 3 cols       */}
             <Page4IMGcontainer imgPosition="left-0" img='pinterest.webp' justify="justify-start" prizeName="Pinterest Fullstack" prize="Express, EJS & Mongodb" link="#" />
-            <Page4IMGcontainer imgPosition="sm:left-0 right-0" img='domidex2.webp' justify="sm:justify-center justify-end" prizeName="Domidex Design" prize="React JS, Tailwind, GSAP" link="https://usman-domidex-design.netlify.app/" />
-            <Page4IMGcontainer imgPosition="left-0" img='ochi.webp' justify="sm:justify-end justify-start" prizeName="Ochi Design" prize="JavaScript, Tailwind, GSAP" link="https://usman-ochi-design.netlify.app/" />
-            <Page4IMGcontainer imgPosition="right-0" img='portfolio.webp' justify="sm:justify-center justify-end" prizeName="Portfolio Template" prize="JavaScript, GSAP, ScrollTrigger" link="https://portfolio-clone-by-usman.netlify.app/" />
-            <Page4IMGcontainer imgPosition="sm:right-0 left-0" img='/Spotify.webp' justify="justify-start" prizeName="Spotify Clone" prize="JavaScrip, Tailwind CSS" link="https://usman-spotify.netlify.app/" />
+            <Page4IMGcontainer imgPosition="right-0 tablet:left-0 tablet:right-auto lgtab:left-0 lgtab:right-auto sm:left-0 sm:right-auto" img='domidex2.webp' justify="justify-start tablet:justify-end lgtab:justify-end sm:justify-center" prizeName="Domidex Design" prize="React JS, Tailwind, GSAP" link="https://usman-domidex-design.netlify.app/" />
+            <Page4IMGcontainer imgPosition="left-0 tablet:right-0 tablet:left-auto lgtab:right-0 lgtab:left-auto sm:left-0 sm:right-auto" img='ochi.webp' justify="justify-start sm:justify-end" prizeName="Ochi Design" prize="JavaScript, Tailwind, GSAP" link="https://usman-ochi-design.netlify.app/" />
+            <Page4IMGcontainer imgPosition="right-0 tablet:left-0 tablet:right-auto lgtab:left-0 lgtab:right-auto sm:right-0 sm:left-auto" img='portfolio.webp' justify="justify-start tablet:justify-end lgtab:justify-end sm:justify-center" prizeName="Portfolio Template" prize="JavaScript, GSAP, ScrollTrigger" link="https://portfolio-clone-by-usman.netlify.app/" />
+            <Page4IMGcontainer imgPosition="left-0 tablet:right-0 tablet:left-auto lgtab:right-0 lgtab:left-auto sm:right-0 sm:left-auto" img='/Spotify.webp' justify="justify-start" prizeName="Spotify Clone" prize="JavaScrip, Tailwind CSS" link="https://usman-spotify.netlify.app/" />
         </div>
     </div>
 )}
